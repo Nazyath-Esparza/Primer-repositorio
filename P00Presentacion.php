6ea2bc1411f9088d5bd0912bc3bfd62cc278592f
Nazyath Esparza <nazyath.esparza.chavez@cbtis89.edu.mx>
2024-10-21 09:28:59 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,12 @@
+<?php
+/* CBTIS 89
+   Programa que imprime mis datos perasonales
+   Nazyath Elianeth Esparza Chávez
+   3°A Programación Matutino
+*/
+echo "MIS DATOS PERSONALES <br><br>";
+echo "Mi nombre es Nazyath Elianeth Esparza Chávez <br>";
+echo "Tengo 16 años <br>";
+echo "Estudio en el CBTIs 89, en la carrera de Programación en tercer semestre del grupo A <br>";
+echo "Nací y vivo en Durango capital";
+?>
