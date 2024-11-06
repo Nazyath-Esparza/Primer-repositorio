cd97940546062d06db326e263c27db01e5a32daf
Nazyath-Esparza <nazyath.esparza.chavez@cbtis89.edu.mx>
2024-11-06 10:51:32 -0600

    Add Carpeta Unidad3
    Los archivos para la tercera unidad

---
+++ Unidad3EsparzaChavez/P37Array17.php
@@ -0,0 +1,40 @@
+<?php
+/*CBTIS 89
+   Programa que por medio de un array que contiene números positivos y negativos, los ordene en 2 arrays distintos
+   Nazyath Elianeth Esparza Chávez
+   3°A Programación Matutino
+*/
+
+$ArrayNumeros = array(-4, 8, 17, -11, -3, 1, 20, -30, 21, 50, -73, -7, -10, -8);
+$ArrayPositivo = array();
+$ArrayNegativo = array();
+$SumaPositivo=0;
+$SumaNegativo=0;
+
+for ($i=0; $i <count($ArrayNumeros); $i++) {
+
+	if ( $ArrayNumeros[$i] >= 0) 
+	{
+	 $ArrayPositivo[] = $ArrayNumeros[$i];
+	 $SumaPositivo += $ArrayNumeros[$i]; 
+	}
+	else {
+	   $ArrayNegativo[] = $ArrayNumeros[$i];
+	   $SumaNegativo += $ArrayNumeros[$i];
+	}
+}
+
+	echo "<P> NÚMEROS POSITIVOS </P>";
+for ($i=0; $i <count($ArrayPositivo); $i++) { 
+	echo $ArrayPositivo[$i];
+	echo "<br>"; 
+}
+echo "<br> LA SUMA DE LOS POSITIVOS ES: $SumaPositivo <br>";
+
+	echo "<P> NÚMEROS NEGATIVOS </P> ";
+for ($i=0; $i <count($ArrayNegativo); $i++) { 
+	echo $ArrayNegativo[$i];
+	echo "<br>"; 
+}
+echo "<br> LA SUMA DE LOS NEGATIVOS ES: $SumaNegativo <br>";
+?>
